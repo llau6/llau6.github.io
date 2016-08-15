43a8e0994d209202a60fd9d3ff019a24451b9244
llau6 <lau.a.lily@gmail.com>
2016-08-15 17:58:53 -0400

    Added content to MULE page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 /**
  * Created by Lily on 8/7/2016.
  */
+$( document ).ready(function() {
+
+    $(".thumbnail").mouseover(function() {
+        $(this).fadeTo( "fast" , 0.5, function() {
+            // Animation complete.
+        });
+    }).mouseout(function() {
+        $(this).fadeTo( "fast" , 1, function() {
+            // Animation complete.
+        });
+    });
+});
 
 
 
